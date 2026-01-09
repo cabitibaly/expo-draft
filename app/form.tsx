e7cd72748d90d6f3b35c025e045842b3ee0204c4
cabitibaly <bitibalyclementalex@gmail.com>
2026-01-09 15:48:52 +0000

    ajout de image-picker

--- app/form.tsx
+++ app/form.tsx
@@ -2,6 +2,7 @@ import CustomDateTimePicker from '@/components/date-time-picker';
 import DatePicker from '@/components/datePicker';
 import Dropdown from '@/components/dropdown';
 import FileUpdoald from '@/components/fileUpdoald';
+import ImagePicker from '@/components/imagePicker';
 import * as DocumentPicker from 'expo-document-picker';
 import React, { useState } from 'react';
 import { ImageBackground } from 'react-native';
@@ -11,21 +12,31 @@ const Form = () => {
     const [selectedDate2, setSelectedDate2] = useState<string>("");
     const [file, setFile] = useState<DocumentPicker.DocumentPickerAsset | null>(null);
     const [time, setTime] = useState<Date>(new Date());
+    const [image, setImage] = useState<string>("");
 
     const defaultTimeFormat = (h: number, m: number): Date => {
         const date = new Date();
         date.setHours(h, m, 0, 0);
         return date;
-    }
-
-    console.log(time);
+    }    
 
     return (
         <ImageBackground
             source={require("../assets/images/main-background.jpg")}
             resizeMode="cover"
             className="px-4 py-4 pt-28 flex-1 items-center justify-center gap-6"
-        >            
+        >       
+            {/* <View className='overflow-hidden size-32 rounded-full bg-turquoise-8 items-center justify-center'>                    
+                {
+                    image ?
+                        <Image className='size-full' source={{ uri: image }} /> 
+                        : 
+                        <Text className='text-gris-12 text-5xl font-bold'>
+                            C
+                        </Text>
+                }
+            </View>      */}
+            <ImagePicker />
             <CustomDateTimePicker onDateChange={setTime} defaultTime={defaultTimeFormat(8, 0)} />
             <DatePicker setSelectedDate={setSelectedDate2} selectedDate={selectedDate2} />                       
             <Dropdown setType={setSelectedDate} type={selectedDate} />                       
---
+++ components/imagePicker.tsx
@@ -0,0 +1,40 @@
+import * as DocumentPicker from 'expo-document-picker';
+import { Camera } from 'lucide-react-native';
+import React, { useState } from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
+
+const ImagePicker = () => {
+    const [file, setFile] = useState<DocumentPicker.DocumentPickerAsset>()
+
+    const pickFile = async () => {
+        const result = await DocumentPicker.getDocumentAsync({
+            type: 'image/*',
+            copyToCacheDirectory: false,
+            multiple: false,
+        })
+
+        if (!result.canceled) {
+            setFile(result.assets[0]);
+        }
+    }
+
+    return (
+        <View className='relative'>
+            <TouchableOpacity onPress={pickFile} activeOpacity={0.8} className=' overflow-hidden size-32 rounded-full bg-turquoise-8 items-center justify-center'>
+                {
+                    file ?
+                        <Image className='size-full' source={{ uri: file.uri }} />
+                        : 
+                        <Text className='text-gris-12 text-5xl font-bold'>
+                            C
+                        </Text>
+                }            
+            </TouchableOpacity>
+            <View className='absolute bottom-0 right-4 size-6 bg-turquoise-8 rounded-full items-center justify-center'>
+                <Camera strokeWidth={2.5} size={14} color={"#EEEEF0"} />
+            </View>
+        </View>
+    )
+}
+
+export default ImagePicker
